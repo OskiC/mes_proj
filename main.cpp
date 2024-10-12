0e3631496e692aa41ccedd410b9f4ab9d2fba779
OskiC <xoskarx1@gmail.com>
2024-10-12 16:55:55 +0200

    update main and add data

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include "structures\GlobalData.h"
+#include "structures\grid.h"
+#include <filesystem>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string fName = "../data/test14.txt";
+
+    oc::GlobalData globalData{};
+    globalData.parseFromFile(fName);
+
+    oc::Grid grid;
+    grid.parseNodes(fName);
+    grid.parseElements(fName);
+
+    globalData.printData();
+
+    std::cout << "\n\n\n\n\n";
+
+    grid.printNodes();
+    grid.printElemets();
+
+    std::cout << std::filesystem::current_path();
     return 0;
 }
